2d65a5212e036197f6077cdc272488b262ef0684
passakornpiy-ctrl <passakornpiy@gmail.com>
2026-04-25 09:55:39 +0700

    th

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 num = int(input())
 if (num == 0):
-    print("This number is equal to zero.")
+    print("This number is different from zero.")
 else:
     print("This number is equal to zero.")
