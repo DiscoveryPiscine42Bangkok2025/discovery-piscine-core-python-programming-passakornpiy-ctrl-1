e195c40712568a6d01f66e9cfaede648e6ba5674
passakornpiy-ctrl <passakornpiy@gmail.com>
2026-04-25 10:19:20 +0700

    2-3

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+num1 = int(input("Enter the first number:\n"))
+num2 = int(input("Enter the second number:\n"))
+result = num1 * num2
+print(f"{num1} x {num2} = {result}")
+if (result > 0):
+    print("This number is positive.")
+elif (result < 0):
+    print("This number is negative.")
+else:
+    print("This number is both positive and negative.")
