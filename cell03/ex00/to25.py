cded1450305b76ff00900e0c41d08d3a8f6016c4
passakornpiy-ctrl <passakornpiy@gmail.com>
2026-04-25 10:43:45 +0700

    3

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+num = int(input("Enter a number less than 25\n"))
+if (num > 25):
+    print("ERROR")
+while (num <= 25):
+    print("Inside the loop, my variable is", num)
+    num += 1
